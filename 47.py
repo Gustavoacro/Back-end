570d822e847b007ccf6fedfb4f5c5d9a84cabb46
Gustavo <gustavosandes57@gmail.com>
2024-03-19 10:12:57 -0300

    create new ativ

---
+++ 47.py
@@ -0,0 +1,6 @@
+for pares in range(1, 50):
+    if pares % 2 == 0:
+        print(pares)
+    #pares += pares
+    #print(pares)
+
---
+++ 48.py
@@ -0,0 +1,4 @@
+for impar in range(1, 500):
+    if impar % 3 == 0:
+       impar += impar
+       print(impar) 
